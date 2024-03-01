11cbd4d4fba4a2cf09646af107dd215a7a1557e6
asahelcode <hoyx0101@gmail.com>
2024-03-01 13:29:13 +0100

    add resolver to query students by session

--- src/graphql/resolvers/session.ts
+++ src/graphql/resolvers/session.ts
@@ -53,8 +53,56 @@ const sessionResolver = {
           }
         }
       })
+    },
+    getSessionFacultyPresidentAndVice: async (_: any, args: any, { prisma }: any) => {
+      const { session } = args
+
+      return prisma.session.findUnique({
+        where: {
+          session
+        },
+        include: {
+          history: {
+          where: {
+              level: 'FACULTY',
+              positionId: {
+                in: ['c85e2fb9-827e-46a7-9cae-498fec337cf7', '74fd8ba6-ddb8-439d-ba8a-aff060c40987']
+              }
+            },
+            include: {
+              department: true,
+              position: true
+            }
+          }
+        }
+      })
+    },
+    getSessionDepartmentPresidentAndVice: async (_: any, args: any, { prisma }: any) => {
+      const { session } = args
+
+      return prisma.session.findMany({
+        where: {
+          session
+        },
+        include: {
+          history: {
+          where: {
+              level: 'DEPARTMENT',
+              positionId: {
+                in: ['c85e2fb9-827e-46a7-9cae-498fec337cf7', '74fd8ba6-ddb8-439d-ba8a-aff060c40987']
+              }
+            },
+            include: {
+              department: true,
+              position: true
+            }
+          }
+        }
+      })
     }
   },
+
+  
   Mutation: {
     createSession: async (_: any, args: any, { prisma }: any) => {
       const { session } = args
