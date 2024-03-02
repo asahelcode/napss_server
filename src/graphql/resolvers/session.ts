1d88e847287c16812e77b19dd51d6a97e2ad3a93
asahelcode <hoyx0101@gmail.com>
2024-03-02 06:09:30 +0100

    add get faculty president and vice president by sesssion value resolver

--- src/graphql/resolvers/session.ts
+++ src/graphql/resolvers/session.ts
@@ -18,6 +18,7 @@ const sessionResolver = {
         }
       })
     },
+    
     sessionFacultyPresidentAndVice: (_: any, __: any, { prisma }: any) => {
       return prisma.session.findMany({
         include: {
@@ -68,6 +69,29 @@ const sessionResolver = {
               positionId: {
                 in: ['c85e2fb9-827e-46a7-9cae-498fec337cf7', '74fd8ba6-ddb8-439d-ba8a-aff060c40987']
               }
+            },
+            include: {
+              department: true,
+              position: true
+            }
+          }
+        }
+      })
+    },
+    getsessionFacultyAndDeptPresident: async (_: any, args: any, { prisma }: any) => {
+      const { session } = args
+
+      return prisma.session.findMany({
+        where: {
+          session
+        },
+        include: {
+          history: {
+            where: {
+              level: {
+                in: ['FACULTY', 'DEPARTMENT']
+              },
+              positionId: 'c85e2fb9-827e-46a7-9cae-498fec337cf7'
             },
             include: {
               department: true,
