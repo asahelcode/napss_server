03b1eca115768cebdaa33d92e5913d659284ac30
asahelcode <hoyx0101@gmail.com>
2024-04-14 17:33:04 +0100

    implementing REST API

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,16 @@
 import express from 'express'
 import cors from 'cors'
-import { ApolloServer } from 'apollo-server-express'
 import { PrismaClient } from '@prisma/client'
-import schema from './graphql/schema'
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 const prisma = new PrismaClient()
 
-const startServer = async (): Promise<void> => {
-  const server = new ApolloServer({
-    schema,
-    context: { prisma },
-    introspection: true
-  })
-
-  await server.start().then(() => { console.log('successfully start graphql server') })
-  server.applyMiddleware({ app, path: '/graphql' })
-}
+app.use('/api/')
 
 const main = async (): Promise<void> => {
-  await startServer()
   app.listen({ port: 4000 }, () => {
     console.log('dev server up')
   })
