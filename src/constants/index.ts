da7f57597382c3479340732f6f6c61e64cb62f92
asahelcode <hoyx0101@gmail.com>
2024-04-18 15:45:42 +0100

    implement napss server to REST

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,4 @@
 const presidentId = 'f6c7d0e8-c8ec-48f4-8d48-5627e490f1d2'
 const vicePresidentId = 'af804803-beb0-4e8f-a56a-74441bd428fb'
 
-// const presidentId = 'b19f79f0-c549-4bf7-96b2-37407df1115c'
-// const vicePresidentId = '8ffa2a48-519f-4df9-8f0f-b60b4a8df106'
-
 export { presidentId, vicePresidentId }
