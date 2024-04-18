b6bc8670c325a60563ba45332ae6ebc830cb53f9
asahelcode <hoyx0101@gmail.com>
2024-04-18 15:52:22 +0100

    done

--- dist/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vicePresidentId = exports.presidentId = void 0;
-const presidentId = 'f6c7d0e8-c8ec-48f4-8d48-5627e490f1d2';
-exports.presidentId = presidentId;
-const vicePresidentId = 'af804803-beb0-4e8f-a56a-74441bd428fb';
-exports.vicePresidentId = vicePresidentId;
--- dist/routes/session.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const session_1 = require("../controllers/session");
-const express_1 = require("express");
-const sessionRouter = (0, express_1.Router)();
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-sessionRouter.get('', session_1.getAllSession);
-exports.default = sessionRouter;
